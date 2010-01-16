752209b9573b81e34a523af3d4cfc2b83157fe16
Eddy Kim <eddyhkim@gmail.com>
2010-01-15 17:27:09 -0800

    Initial commit to sass_hsl_ext.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'sass_hsl_ext'
+
+class Test::Unit::TestCase
+end
